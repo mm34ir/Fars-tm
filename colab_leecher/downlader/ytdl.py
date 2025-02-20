547faec9475aa8bc60b621db2e74a061619d3ae1
mm34ir <76752793+mm34ir@users.noreply.github.com>
2025-02-20 14:05:32 +0330

    Update ytdl.py

--- colab_leecher/downlader/ytdl.py
+++ colab_leecher/downlader/ytdl.py
@@ -96,7 +96,7 @@ def YouTubeDL(url):
             logging.info(d)
 
     ydl_opts = {
-        "format": "best",
+        "format": "best[height=480]/best[height=720]/best",
         "allow_multiple_video_streams": True,
         "allow_multiple_audio_streams": True,
         "writethumbnail": True,
